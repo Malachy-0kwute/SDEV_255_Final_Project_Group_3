1f047465de4fac3a90d8e5982fde625d2f0145d6
malachy okwute <mokwute@ivytech.edu>
2026-07-13 21:48:06 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig(({command}) => ({
   plugins: [react()],
-  base: command === 'build' ? '/SDEV_255_Final_Project_Group_3/' : '/',
+  // base: command === 'build' ? '/SDEV_255_Final_Project_Group_3/' : '/',
+  base: '/SDEV_255_Final_Project_Group_3/',
   build: {
     outDir: 'docs',
   },
