e4c184308425c6c0ed136ee5b2974ccaddd47437
malachy okwute <mokwute@ivytech.edu>
2026-07-13 21:35:47 -0400

    Configure Vite to build to docs folder for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({command}) => ({
   plugins: [react()],
-  base: '/SDEV_255_Final_Project_Group_3/',
-})
+  base: command === 'build' ? '/SDEV_255_Final_Project_Group_3/' : '/',
+  build: {
+    outDir: 'docs',
+  },
+}));
